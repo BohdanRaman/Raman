d1abefe8aeb6c61441c1cd66fdaec0029b9fa0ae
Bohdan Raman <bohdanraman@gmail.com>
2022-09-20 00:35:13 +0300

    Resolve problem with reade/write in Main.

--- src/main/java/helper/Reader.java
+++ src/main/java/helper/Reader.java
@@ -1,24 +1,28 @@
 package helper;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Reader {
     public Reader(String path) {
+        this.path = path;
     }
 
-    public void readFile(){
-        String path = "School.txt";
+    private final String path;
+
+    public void readFile() {
+
         try {
             File file = new File(path);
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                System.out.println(scanner.nextLine());
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            String line = reader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = reader.readLine();
             }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Error");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
